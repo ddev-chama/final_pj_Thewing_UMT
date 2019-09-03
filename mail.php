b10c49d55d1a9e3ee82872c03cc88c17bfe80400
Admin <ddev.chama@gmail.com>
2019-09-03 15:30:29 +0700

    day2 wait for Done

--- mail.php
+++ mail.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * This example shows making an SMTP connection with authentication.
- */
+$s = 1;
+if(empty($_POST["UtypeB"] || $_POST["UPhone"] || $_POST["Uemail"] || $_POST["Uname"])){ 
+    header("location:javascript:history.go(-1)");
+}
+else
+{//start_else
+$client_msg_name =  $_POST["Uname"] ;
+$client_msg_mail = $_POST["Uemail"]; 
+$client_msg_phone = $_POST["UPhone"] ;
+$client_msg_business = $_POST["UtypeB"] ;
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
@@ -33,19 +40,22 @@ $mail->Username = "ddev.chama@gmail.com";
 //Password to use for SMTP authentication
 $mail->Password = "Earth123@";
 //Set who the message is to be sent from
-$mail->setFrom('ddev.chama@gmail.com', 'Tony jone');
+$mail->setFrom('ddev.chama@gmail.com', 'Thewing_Alert');
 //Set who the message is to be sent to
-$mail->addAddress('ak3167@gmail.com', 'itoffside');
+$mail->addAddress('ak3167@gmail.com', 'Chama');
 //Set the subject line
-$mail->Subject = 'itOffside.com test email';
+$mail->Subject = 'Message from '.$client_msg_name;
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
 //$mail->msgHTML(file_get_contents('content.html'), dirname(__FILE__));
-$mail->msgHTML("Test email by itoffside.com");
+$mail->msgHTML("ผู้สนใจ :".$client_msg_name."<br>ประเภทธุรกิจ :".$client_msg_business."<br>เบอร์ติดต่อ :".$client_msg_phone."<br>Email Address :".$client_msg_mail);
 
 //send the message, check for errors
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
     echo "Message sent!";
+    header("location:index.php?status=success")
 }
+
+}//end_else
